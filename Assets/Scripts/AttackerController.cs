b445604963442696587b9dd9fbc3e553d78872cd
Joojinho13 <joojvitor13@gmail.com>
2024-01-26 14:22:13 -0300

    Versão 0.2 (Menu Update)

--- Assets/Scripts/AttackerController.cs
+++ Assets/Scripts/AttackerController.cs
@@ -1,37 +1,48 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AttackerController : MonoBehaviour
 {
-
     public GameObject ball;
-    public GameObject playerCamera;
+    public GameObject ballThrowingPoint;
 
     public bool holdingBall = true;
 
-    public float ballDistance;
     public float ballThrowingForce;
+    public float maxThrowingForce = 100f;
+    public float accuracyFactor = 0.2f; // Fator de precisão, ajuste conforme necessário
+    public float randomFactor = 0.1f;   // Fator aleatório para adicionar variabilidade
 
+    private Vector3 dragStartPosition;
+    private Vector3 dragEndPosition;
 
     void Start()
     {
-        ball.GetComponent<Rigidbody> ().useGravity = false;
-
+        ball.GetComponent<Rigidbody>().useGravity = false;
     }
 
     void Update()
     {
-        if(holdingBall)
+        if (holdingBall)
         {
-        ball.transform.position = playerCamera.transform.position + playerCamera.transform.forward * ballDistance;
-
             if (Input.GetMouseButtonDown(0))
+            {
+                dragStartPosition = Input.mousePosition;
+            }
+
+            if (Input.GetMouseButtonUp(0))
             {
                 holdingBall = false;
                 ball.GetComponent<Rigidbody>().useGravity = true;
-                ball.GetComponent<Rigidbody>().AddForce (playerCamera.transform.forward * ballThrowingForce);
+
+                dragEndPosition = Input.mousePosition;
+                Vector3 dragDirection = (dragEndPosition - dragStartPosition).normalized;
+
+                float throwingForce = Mathf.Clamp((dragEndPosition - dragStartPosition).magnitude, 0f, maxThrowingForce);
+                throwingForce *= 1 + Random.Range(-randomFactor, randomFactor); // Adiciona variabilidade
+
+                // Aplica força ao arremessar, levando em consideração a precisão
+                ball.GetComponent<Rigidbody>().AddForce(dragDirection * throwingForce * accuracyFactor);
             }
         }
     }
--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
+using System.Linq;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    public static Launcher Instance;
+
     [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
+    [SerializeField] Transform roomListContent;
+    [SerializeField] GameObject roomListItemPrefab;
+    [SerializeField] Transform playerListContent;
+    [SerializeField] GameObject PlayerListItemPrefab;
+    [SerializeField] GameObject startGameButton;
+
+    public object RoomList { get; private set; }
+
+    void Awake()
+    {
+        Instance = this;
+    }
 
     void Start()
     {
@@ -18,12 +36,20 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         Debug.Log("Conectado à Master");
         PhotonNetwork.JoinLobby();
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
 
     public override void OnJoinedLobby()
     {
-        MenuManager.Instance.OpenMenu("title");
+        MenuManager.Instance.OpenMenu("Title");
         Debug.Log("juntou-se ao Lobby");
+        PhotonNetwork.NickName = "Player " + Random.Range(0, 1000).ToString("0000");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Jogo fechado!");
+        Application.Quit();
     }
 
     public void CreateRoom()
@@ -33,15 +59,78 @@ public class Launcher : MonoBehaviourPunCallbacks
             return;
         }
         PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.Instance.OpenMenu("Loading");
     }
 
     public override void OnJoinedRoom()
     {
+        MenuManager.Instance.OpenMenu("Room");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
 
+        Player[] players = PhotonNetwork.PlayerList;
+
+        foreach(Transform child in playerListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < players.Count(); i++)
+        {
+            if (players[i].IsInactive)
+            {
+                continue;
+            }
+            Instantiate(PlayerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
+        }
+
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
+        errorText.text = "Falha ao Criar a Sala: " + message;
+        MenuManager.Instance.OpenMenu("Error");
+    }
 
+    public void StartGame()
+    {
+        PhotonNetwork.LoadLevel(1);
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("Loading");
+    }
+
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+        MenuManager.Instance.OpenMenu("Loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("Title");
+
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach(Transform trans in roomListContent)
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Instantiate(PlayerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
     }
 }
