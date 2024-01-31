8b26520a818d20dfe402f75580262bb4e0fffeef
João Victor <joojvitor13@gmail.com>
2024-01-30 21:17:26 -0300

    Arremesso modificado

--- Assets/Scripts/AttackerController.cs
+++ Assets/Scripts/AttackerController.cs
@@ -18,6 +18,7 @@ public class AttackerController : MonoBehaviour
 
     void Update()
     {
+       
         if (holdingBall)
         {
             if (Input.GetMouseButtonDown(0))
@@ -36,14 +37,13 @@ public class AttackerController : MonoBehaviour
                 float throwingForce = Mathf.Clamp((dragEndPosition - dragStartPosition).magnitude, 0f, maxThrowingForce);
                 throwingForce *= 1 + Random.Range(-randomFactor, randomFactor);
 
-                // Ajuste adicional para o arremesso ir para cima e para frente
                 float upwardForce = throwingForce * 0.5f; // Ajuste conforme necessário
                 float forwardForce = throwingForce * 2f; // Ajuste conforme necessário
 
-                Vector3 throwForce = new Vector3(dragDirection.x * forwardForce, upwardForce, dragDirection.y * forwardForce);
+                Vector3 throwForce = new Vector3(dragDirection.x * forwardForce * 50, upwardForce * 50, dragDirection.y * forwardForce* 50);
 
                 // Aplica força ao arremessar, levando em consideração a precisão
-                ball.GetComponent<Rigidbody>().AddForce(throwForce * accuracyFactor);
+                ball.GetComponent<Rigidbody>().AddForce((throwForce * 100) * accuracyFactor);
                 
                 Debug.Log("Throw Direction: " + dragDirection);
                 Debug.Log("Throw Force: " + throwingForce);
